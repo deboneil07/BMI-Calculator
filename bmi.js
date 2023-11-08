00af9b938100330447d51fa5a039681e1f4d0df3
Deboneil <bhattacharjeedeboneil@gmail.com>
2023-11-08 21:24:28 +0530

    full functional bmi index

--- bmi.js
+++ bmi.js
@@ -1,6 +1,11 @@
+
 function result(){
     weight = parseFloat(document.getElementById("inp").value);
     height = parseFloat(document.getElementById('inp2').value);
-    output = (weight + height);
-    alert(output + " KG");
+    answer = weight / (height * height);
+    document.getElementById("inp3").value = answer.toFixed(2) + " BMI";
+
+    
+
+    
 }
