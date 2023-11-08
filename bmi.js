1f7a0e3ac2afa7c56418e816dbe8d1569c14a30d
Deboneil <bhattacharjeedeboneil@gmail.com>
2023-11-08 21:51:30 +0530

    color change

--- bmi.js
+++ bmi.js
@@ -4,8 +4,16 @@ function result(){
     height = parseFloat(document.getElementById('inp2').value);
     answer = weight / (height * height);
     document.getElementById("inp3").value = answer.toFixed(2) + " BMI";
+    final_answer = answer.toFixed(2);
+    if (final_answer < 20){
+         const result_box = document.getElementById("card");
+         result_box.style.backgroundColor = "#EF0107";
+         result_box.style.boxShadow = "none"
+         
 
-    
+    }
+
+
 
     
 }
