cd44dbf35050e53a86793d9ec4d5586bd661143a
Deboneil <bhattacharjeedeboneil@gmail.com>
2023-11-09 18:11:18 +0530

    added reset button

--- bmi.js
+++ bmi.js
@@ -1,19 +1,33 @@
+const reset_button = document.getElementById("reset");
 
-function result(){
-    weight = parseFloat(document.getElementById("inp").value);
-    height = parseFloat(document.getElementById('inp2').value);
+function result() {
+  weight = parseFloat(document.getElementById("inp").value);
+  height = parseFloat(document.getElementById("inp2").value);
+  
+
+  if (weight && height) {
     answer = weight / (height * height);
     document.getElementById("inp3").value = answer.toFixed(2) + " BMI";
     final_answer = answer.toFixed(2);
-    if (final_answer < 20){
-         const result_box = document.getElementById("card");
-         result_box.style.backgroundColor = "#EF0107";
-         result_box.style.boxShadow = "none"
-         
 
+    const result_box = document.getElementById("card");
+    const result_button = document.getElementById("inp_click");
+    result_box.style.backgroundColor = "#EF0107";
+    result_box.style.boxShadow = "none";
+    result_button.style.color = "#EF0107";
+    result_button.style.borderColor = "#EF0107";
+  } else {
+    if (weight === 0 || height === 0) {
+      document.getElementById("inp3").value = "Error.";
     }
 
+    document.getElementById("inp3").value = "Error.";
+  }
 
 
-    
+}
+
+function reset(){
+    document.getElementById("inp").value= "";
+    document.getElementById("inp2").value = "";
 }
